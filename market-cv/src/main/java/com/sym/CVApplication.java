b7f6a10fca03a391487d10136f5bab7fcc8656e3
suyongming <476231418@qq.com>
2019-08-28 16:47:36 +0800

    cloud升级

--- market-cv/src/main/java/com/sym/CVApplication.java
+++ market-cv/src/main/java/com/sym/CVApplication.java
@@ -1,14 +1,5 @@
-/**
- * Copyright (c) 2016-2019 人人开源 All rights reserved.
- *
- * https://www.renren.io
- *
- * 版权所有，侵权必究！
- */
-
 package com.sym;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
