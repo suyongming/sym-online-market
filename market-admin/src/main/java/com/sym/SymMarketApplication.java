72e6364dd761138659b915df3059e53055fd1636
suyongming <476231418@qq.com>
2019-08-30 10:00:28 +0800

    cloud 暂缓

--- market-admin/src/main/java/com/sym/SymMarketApplication.java
+++ market-admin/src/main/java/com/sym/SymMarketApplication.java
@@ -10,13 +10,13 @@ package com.sym;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
+//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+//import org.springframework.cloud.openfeign.EnableFeignClients;
 
 
 @SpringBootApplication
-@EnableDiscoveryClient
-@EnableFeignClients
+//@EnableDiscoveryClient
+//@EnableFeignClients
 public class SymMarketApplication {
 
 	public static void main(String[] args) {
--- market-admin/src/main/java/com/sym/modules/order/controller/OrderMasterController.java
+++ market-admin/src/main/java/com/sym/modules/order/controller/OrderMasterController.java
@@ -35,7 +35,7 @@ public class OrderMasterController {
      * 列表
      */
     @RequestMapping("/list")
-    @RequiresPermissions("order:ordermaster:list")
+//    @RequiresPermissions("order:ordermaster:list")
     public R list(@RequestParam Map<String, Object> params){
         PageUtils page = orderMasterService.queryPage(params);
 
