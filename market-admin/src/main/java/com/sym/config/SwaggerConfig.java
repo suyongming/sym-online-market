de7835eae27cb15045a70b0ed76a30affeeb7d48
suyongming <476231418@qq.com>
2020-01-18 21:26:20 +0800

    退回 正确配置

--- market-admin/src/main/java/com/sym/config/SwaggerConfig.java
+++ market-admin/src/main/java/com/sym/config/SwaggerConfig.java
@@ -39,10 +39,10 @@ public class SwaggerConfig implements WebMvcConfigurer {
 
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
-            .title("人人开源")
-            .description("renren-fast文档")
-            .termsOfServiceUrl("https://www.renren.io")
-            .version("3.0.0")
+            .title("SYM")
+            .description("SYM-MARKET文档")
+            .termsOfServiceUrl("https://github.com/suyongming")
+            .version("1.0.0")
             .build();
     }
 
